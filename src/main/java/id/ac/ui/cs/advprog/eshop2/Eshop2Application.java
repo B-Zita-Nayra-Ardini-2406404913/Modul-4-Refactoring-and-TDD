c72fbf77ae370ed5a566ee253c0e45110e257be7
nayraarr <zita.nayra.ardini@gmail.com>
2026-03-08 09:51:09 +0700

    Initial project

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop2/Eshop2Application.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.eshop2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eshop2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Eshop2Application.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/eshop2/Eshop2ApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.eshop2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eshop2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
